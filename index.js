c7160999dc8f916165323dd41074063d3fad947b
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-07-09 23:47:09 +1000

    Created GUI

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Get computers choice
 function getComputerChoice() {
     let a = Math.floor(Math.random()*3);
     switch (a) {
@@ -14,6 +15,7 @@ function getComputerChoice() {
     return a;
 }
 
+//Plays the game returns who won
 function playGame(computerChoice, playerChoice) {
     if (computerChoice.toLowerCase() === playerChoice.toLowerCase()) {
       return `Draw! Both chose ${playerChoice}`;
@@ -38,32 +40,76 @@ function playGame(computerChoice, playerChoice) {
     }
 }
 
+//player choice funtion
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let playerChoice = button.id;
+    game(playerChoice)
+  });
+});
+
+//Function for getting who won after a number of games
+
+//Declare initial vairables
 let computerWins = 0;
 let playerWins = 0;
 let draws = 0
 
-function game() {
-    for (i = 0; i < 5; ++i) {
-        let playerChoice = prompt('Rock, Paper, Scissors, Shoot...');
-        let computerChoice = getComputerChoice();
-        let result = playGame(computerChoice, playerChoice);
-        console.log(result);
-        if (result.charAt(4) === 'W') {
-            playerWins++;
-        } else if (result.charAt(4) === 'L') {
-            computerWins++;
-        } else {
-            draws++;
-        }
-    }
-    if (computerWins > playerWins) {
-        return `Computer Wins with ${computerWins} Wins, ${playerWins} loses and ${draws} draws!`
-    } else if (playerWins > computerWins) {
-        return `You Win! With ${playerWins} Wins, ${computerWins} losss and ${draws} draws!`
-    } else {
-        return `Draw!`
-    }
+function game(choice) {
+    //Sets player choice to variable and gets cmputer choice
+    let playerChoice = choice;
+    let computerChoice = getComputerChoice();
 
+    //Shows this in the choises span
+    let choices = document.getElementById('choices')
+    choices.textContent = `${playerChoice} vs ${computerChoice}`;
+
+    //Gets result of game
+    let result = playGame(computerChoice, playerChoice);
+
+    //Edits span to show result on Screen
+    let winner = document.getElementById('winner');
+    winner.textContent = result;
+
+    //Increments results
+    if (result.charAt(4) === 'W') {
+        playerWins++;
+    } else if (result.charAt(4) === 'L') {
+        computerWins++;
+    } else {
+        draws++;
+    }
+    //Shows current score on DOM
+    let yourScore = document.getElementById('you')
+    yourScore.textContent = `You: ${playerWins}`;
+
+    let compScore = document.getElementById('computer')
+    compScore.textContent = `Computer: ${computerWins}`;
+
+    let drawScore = document.getElementById('draws')
+    drawScore.textContent = `Draws ${draws}`;
+
+    //Computes winner after 5 games
+    let finalWinner = document.getElementById('finalScore')
+    if (computerWins + playerWins + draws === 5) {
+        if (computerWins > playerWins) {
+            finalWinner.textContent = `Computer Wins with ${computerWins} Wins, ${playerWins} loses and ${draws} draws!`;
+        } else if (playerWins > computerWins) {
+            finalWinner.textContent = `You Win! With ${playerWins} Wins, ${computerWins} losss and ${draws} draws!`;
+        } else {
+            finalWinner.textContent = `Draw!`;
+        }
+        
+    }
 }
 
-console.log(game());
+
+
+//Add Home screen to users can selct 3, 5 or seven games
+//Stop at users slection
+//Add images to choices
+//makes image that wins move into the losing image
+//set timeput fr win/loss/draw messagw
+//make popup for game end - display stats and play again option, home option
