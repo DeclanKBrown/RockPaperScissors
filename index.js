44aa33eafc67946e03f2417d3b483ca0c9b72c6e
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-07-10 17:31:15 +1000

    Added home page // made pretty

--- index.js
+++ index.js
@@ -1,115 +1,53 @@
-//Get computers choice
-function getComputerChoice() {
-    let a = Math.floor(Math.random()*3);
-    switch (a) {
-        case 0:
-            a = 'paper';
-            break;
-        case 1:
-            a = 'scissors';
-            break;
-        case 2:
-            a = 'rock';
-            break;
-    }
-    return a;
-}
+//Home Page 
 
-//Plays the game returns who won
-function playGame(computerChoice, playerChoice) {
-    if (computerChoice.toLowerCase() === playerChoice.toLowerCase()) {
-      return `Draw! Both chose ${playerChoice}`;
-    } else if (computerChoice.toLowerCase() === 'paper') {
-        if (playerChoice.toLowerCase() === 'scissors') {
-            return 'You Win! Scissors beats paper';
-        } else {
-            return 'You Lose! Paper beats rock';
-        }
-    } else if (computerChoice.toLowerCase() === 'scissors') {
-        if (playerChoice.toLowerCase() === 'rock') {
-            return 'You Win! Rock beats scissors';
-        } else {
-            return 'You Lose! Scissors beats paper';
-        }
-    } else if (computerChoice.toLowerCase() === 'rock') {
-        if (playerChoice.toLowerCase() === 'paper') {
-            return 'You Win! Paper beats rock';
-        } else {
-            return 'You Lose! Rock beats scissors';
-        }
-    }
-}
+//Function to get number of games
+const numGamesBtn = document.querySelectorAll('.btns')
 
-//player choice funtion
-const buttons = document.querySelectorAll('button');
+//Buttons click events
+numGamesBtn.forEach((button) => {
+    button.addEventListener('click', () => {
+        //get num games from id
+        let numGames = button.id;
 
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    let playerChoice = button.id;
-    game(playerChoice)
-  });
+        //Set to local storage 
+        localStorage.setItem("numGames", numGames);
+
+        //Call active button function 
+        activateButton({button: numGames});
+
+        //Remove class error from play 
+        const playGameButton = document.querySelector('#play')
+        document.getElementById(playGameButton.id).classList.remove('error')
+        document.getElementById(playGameButton.id).classList.add('active')
+    });
 });
 
-//Function for getting who won after a number of games
+// button = #id 
 
-//Declare initial vairables
-let computerWins = 0;
-let playerWins = 0;
-let draws = 0
+const activateButton = ({ button }) => {
+    const button1 = '3'
+    const button2 = '5'
+    const button3 = '7'
 
-function game(choice) {
-    //Sets player choice to variable and gets cmputer choice
-    let playerChoice = choice;
-    let computerChoice = getComputerChoice();
+    // reset buttons
+    document.getElementById(button1).classList.remove('active')
+    document.getElementById(button2).classList.remove('active')
+    document.getElementById(button3).classList.remove('active')
 
-    //Shows this in the choises span
-    let choices = document.getElementById('choices')
-    choices.textContent = `${playerChoice} vs ${computerChoice}`;
-
-    //Gets result of game
-    let result = playGame(computerChoice, playerChoice);
-
-    //Edits span to show result on Screen
-    let winner = document.getElementById('winner');
-    winner.textContent = result;
-
-    //Increments results
-    if (result.charAt(4) === 'W') {
-        playerWins++;
-    } else if (result.charAt(4) === 'L') {
-        computerWins++;
-    } else {
-        draws++;
-    }
-    //Shows current score on DOM
-    let yourScore = document.getElementById('you')
-    yourScore.textContent = `You: ${playerWins}`;
-
-    let compScore = document.getElementById('computer')
-    compScore.textContent = `Computer: ${computerWins}`;
-
-    let drawScore = document.getElementById('draws')
-    drawScore.textContent = `Draws ${draws}`;
-
-    //Computes winner after 5 games
-    let finalWinner = document.getElementById('finalScore')
-    if (computerWins + playerWins + draws === 5) {
-        if (computerWins > playerWins) {
-            finalWinner.textContent = `Computer Wins with ${computerWins} Wins, ${playerWins} loses and ${draws} draws!`;
-        } else if (playerWins > computerWins) {
-            finalWinner.textContent = `You Win! With ${playerWins} Wins, ${computerWins} losss and ${draws} draws!`;
-        } else {
-            finalWinner.textContent = `Draw!`;
-        }
-        
-    }
+    document.getElementById(button).classList.add('active')
 }
 
 
-
-//Add Home screen to users can selct 3, 5 or seven games
-//Stop at users slection
-//Add images to choices
-//makes image that wins move into the losing image
-//set timeput fr win/loss/draw messagw
-//make popup for game end - display stats and play again option, home option
+///Function to take user to next page once play is clikced
+const playGameButton = document.querySelector('#play')
+playGameButton.addEventListener('click', () => {
+    //Checks if numGames has been selected
+    for (const btn of numGamesBtn) {
+        if (btn.classList.contains('active')) {
+            location.href = 'play.html';
+        }
+        else {
+            playGameButton.classList.add('error')
+        }
+    }
+})
---
+++ play.js
@@ -0,0 +1,203 @@
+//Get computers choice
+function getComputerChoice() {
+    let a = Math.floor(Math.random()*3);
+    switch (a) {
+        case 0:
+            a = 'paper';
+            break;
+        case 1:
+            a = 'scissors';
+            break;
+        case 2:
+            a = 'rock';
+            break;
+    }
+    return a;
+}
+
+//Plays the game returns who won
+function playGame(computerChoice, playerChoice) {
+    if (computerChoice.toLowerCase() === playerChoice.toLowerCase()) {
+      return `Draw! Both chose ${playerChoice}`;
+    } else if (computerChoice.toLowerCase() === 'paper') {
+        if (playerChoice.toLowerCase() === 'scissors') {
+            return 'You Win! Scissors beats paper';
+        } else {
+            return 'You Lose! Paper beats rock';
+        }
+    } else if (computerChoice.toLowerCase() === 'scissors') {
+        if (playerChoice.toLowerCase() === 'rock') {
+            return 'You Win! Rock beats scissors';
+        } else {
+            return 'You Lose! Scissors beats paper';
+        }
+    } else if (computerChoice.toLowerCase() === 'rock') {
+        if (playerChoice.toLowerCase() === 'paper') {
+            return 'You Win! Paper beats rock';
+        } else {
+            return 'You Lose! Rock beats scissors';
+        }
+    }
+}
+
+//Declare initial vairables
+let computerWins = 0;
+let playerWins = 0;
+let draws = 0
+
+//Function for getting who won after a number of games
+function game(choice, num) {
+    //Sets player choice to variable and gets cmputer choice
+    let playerChoice = choice;
+    let numGames = parseInt(num);
+    let computerChoice = getComputerChoice();
+
+    //Shows this in the choises span
+    let choices = document.getElementById('choices')
+    choices.textContent = `${playerChoice} vs ${computerChoice}`;
+
+    //Gets result of game
+    let result = playGame(computerChoice, playerChoice);
+
+    //Edits span to show result on Screen
+    let winner = document.getElementById('winner');
+    function showResult() {
+        winner.textContent = result
+    }
+    setTimeout(showResult, 500);
+
+    //Increments results
+    if (result.charAt(4) === 'W') {
+        playerWins++;
+    } else if (result.charAt(4) === 'L') {
+        computerWins++;
+    } else {
+        draws++;
+    }
+
+    //Shows current score on DOM
+    let yourScore = document.getElementById('you')
+    let compScore = document.getElementById('computer')
+    let drawScore = document.getElementById('draw')
+
+    function showScore() {
+        yourScore.textContent = `You: ${playerWins}`;
+        compScore.textContent = `Computer: ${computerWins}`;
+        drawScore.textContent = `Draws ${draws}`;
+    }
+    setTimeout(showScore, 500);
+
+    //Displays results of funal score
+    function finalScore() {
+        //create overlay
+        const overlay = document.createElement('div');
+        overlay.classList.add('overlay');
+
+        const popup = document.createElement('div');
+        popup.classList.add('popup');
+
+        const popupHeading = document.createElement('div');
+        popupHeading.classList.add('popupHeading');
+
+        const popupH2 = document.createElement('h2');
+        popupH2.classList.add('popupH2');
+
+        const popupH3 = document.createElement('h3');
+        popupH3.classList.add('popupH3')
+
+        const popupButtons = document.createElement('div');
+        popupButtons.classList.add('popupButtons');
+
+        const playAgainBtn = document.createElement('button');
+        playAgainBtn.classList.add('playAgainBtn');
+        playAgainBtn.innerHTML = 'play again';
+        playAgainBtn.addEventListener('click', () => {
+            location.href = 'play.html';
+        })
+
+        const homeBtn = document.createElement('button');
+        homeBtn.classList.add('homeBtn');
+        homeBtn.textContent = 'home';
+        homeBtn.addEventListener('click', () => {
+            location.href = 'index.html';
+        })
+
+        document.body.appendChild(overlay);
+        overlay.appendChild(popup);
+        popup.appendChild(popupHeading);
+        popupHeading.appendChild(popupH2);
+        popupHeading.appendChild(popupH3)
+        popup.appendChild(popupButtons);
+        popupButtons.appendChild(playAgainBtn);
+        popupButtons.appendChild(homeBtn);
+
+        //Computes who won and displays it on the overlay
+        if (computerWins > playerWins) {
+            popupH2.textContent = `Computer Wins`
+            popupH3.textContent =  `${computerWins} Wins, ${playerWins} loses and ${draws} draws!`;
+        } else if (playerWins > computerWins) {
+            popupH2.textContent = `You Win!`
+            popupH3.textContent =  `${playerWins} Wins, ${computerWins} loses and ${draws} draws!`;
+        } else {
+            popupH2.textContent = `Draw!`;
+            popupH3.textContent =  `${playerWins} Wins, ${computerWins} loses and ${draws} draws!`;
+        }
+    }
+    
+    //Calls finalScore function when num games is reached
+    if (computerWins + playerWins + draws === numGames) {
+        setTimeout(finalScore, 500);
+    }
+}
+
+
+//player choice funtion
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let playerChoice = button.id;
+
+    //Set button style to active
+    activateButton({button: playerChoice});
+
+    //get from local storage
+    let numGames = localStorage.getItem('numGames')
+
+    //Activate game
+    game(playerChoice, numGames)
+  });
+});
+
+//highlight button clikced 
+const activateButton = ({ button }) => {
+    const button1 = 'rock'
+    const button2 = 'paper'
+    const button3 = 'scissors'
+
+    //disables button until result displayed
+    const btn1 = document.getElementById(button1)
+    const btn2 = document.getElementById(button2)
+    const btn3 = document.getElementById(button3)
+    btn1.disabled = true
+    btn2.disabled = true
+    btn3.disabled = true
+    setTimeout(function() {btn1.disabled = false; btn2.disabled = false; btn3.disabled = false;}, 500);
+
+    // reset buttons
+    document.getElementById(button1).classList.remove('active')
+    document.getElementById(button2).classList.remove('active')
+    document.getElementById(button3).classList.remove('active')
+
+    document.getElementById(button).classList.add('active')
+    setTimeout(function() {document.getElementById(button).classList.remove('active')}, 500)
+}
+
+
+
+//Add Home screen to users can selct 3, 5 or seven games
+//Stop at users slection
+//Add images to choices
+//makes image that wins move into the losing image
+//set timeput fr win/loss/draw messagw
+//make popup for game end - display stats and play again option, home option
